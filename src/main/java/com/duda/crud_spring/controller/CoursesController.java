9091894cd6e9644e2d5d0c804b9509923ef0cd97
Maria Eduarda Vieira Monteiro <maria.monteiro@tse.jus.br>
2024-10-11 14:31:24 -0300

    backEnd
    primeiro commit

---
+++ src/main/java/com/duda/crud_spring/controller/CoursesController.java
@@ -0,0 +1,10 @@
+package com.duda.crud_spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CoursesController {
+    
+}
